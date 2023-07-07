d5a5c2975022d0e25adc0d2f91b8a2f7117a1dc8
Raihanul islam <97145938+Raihanulislam12s@users.noreply.github.com>
2023-07-07 21:38:18 +0600

    Add files via upload

---
+++ Problem slove/A_Vasya_the_Hipster.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pi (3.141592653589)
+#define mod 1000000007
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ss second
+#define all(c) c.begin(), c.end()
+#define f0(i, n) for (int i = 0; i < n; ++i)
+#define f1(i, n) for (int i = 1; i < n; ++i)
+#define f2(i, n) for (int i = 1; i <= n; ++i)
+#define f3(i, n) for (int i = n - 1; i >= 0; i--)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+/*-----LCM------*/
+/*int lcm(int a,int b){
+    return a*b/__gcd(a,b);
+}*/
+
+/* Prime Number */
+/*bool isPrime(int n){
+    if(n==1) return false;
+    if(n==2) return true;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0)return false;
+    }
+    return true;
+}*/
+
+/*==Sieve of Eratosthenes==*/
+/*int prime(long long n){
+    if (n < 2)
+        return 0;
+    if (n == 2)
+        return 1;
+    if (n % 2 == 0)
+        return 0;
+    for (long long i = 3; i <= sqrt(n); i += 2)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}*/
+
+/* Main */
+int32_t main()
+{
+    fast int a, b;
+    cin >> a >> b;
+    if (a > b)
+    {
+        cout << b << " ";
+    }
+    else
+    {
+        cout << a << " ";
+    }
+    int ans = abs(a - b);
+    cout << ans / 2;
+    return 0;
+}
